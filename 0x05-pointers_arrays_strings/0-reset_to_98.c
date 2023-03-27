f5a4d942871550ee568d3b3eda8c2805d5a906e8
Martha Ndungu <martha.ndungu1389@gmail.com>
2023-03-27 08:13:51 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer to an int as para
+ * updateing the value it points to 98
+ * @n: input
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
